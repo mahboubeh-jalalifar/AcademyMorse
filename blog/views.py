e69ac76745afd18c2cde1677320a214ddfb8ea19
Mahboubeh <Mahboubeh.jalalifar@gmail.com>
2025-07-10 13:49:24 +0330

    Blog_Datetime

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from datetime import datetime
 import pytz
 def Iran_time(request):
-    Iran_timezone=pytz.timezone("Asia/Tehran")
+    Iran_timezone= pytz.timezone("Asia/Tehran")
     now=datetime.now (Iran_timezone)
     current_time=now.strftime("%Y-%m-%d %H:%M:%S")
     return HttpResponse (f"Today is: {current_time}")
