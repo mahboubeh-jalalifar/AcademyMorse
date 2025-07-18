8f2a1f4eddbe66029ac1d043b53723252f683787
Mahboubeh <Mahboubeh.jalalifar@gmail.com>
2025-07-18 15:15:09 +0330

    Blog.Model

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,23 @@
+from datetime import timezone
 from django.db import models
+class Blog(models.Model):
+    post= models.CharField ()
+    title= models.CharField (max_length=80)
+    content= models.TextField (max_length=200)
+    author= models.ForeignKey ()
+    category= models.CharField (max_length= 20)
+    comment=models.CharField ()
+    created_at= models.DateTimeField (auto_now_add=True)
+    updated_at= models.DateTimeField (auto_now=True)
+    published_at= models.DateTimeField (default=timezone.now)
+    status= models.CharField (choices="STATUS_CHOICES", defult="Published")
+    def __str__(self):
+        return f"{self.title}{self.author}{self.updated_at}"
+    
+class Blog(models.Model):
+    STATUS_CHOICES=[("Published","Published"),
+                    ("Draft","Draft")]
 
+    
 
 # Create your models here.
